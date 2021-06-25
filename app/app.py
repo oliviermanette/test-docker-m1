675f5658c49528eda66c8c36f2b8afab1a3c724b
Lirone Samoun <lirone.samoun@gmail.com>
2021-06-25 15:42:23 +0200

    update package

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
 import time
 
 from pymongo import MongoClient
@@ -10,9 +9,6 @@ application = Flask(__name__)
 application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + \
     os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + \
     ':27017/' + os.environ['MONGODB_DATABASE']
-
-mongo = PyMongo(application)
-db = mongo.db
 
 
 @application.route('/')
@@ -33,36 +29,3 @@ def index():
         status=True,
         message='Welcome to the Dockerized Flask MongoDB app!'
     )
-
-
-@application.route('/todo')
-def todo():
-    _todos = db.todo.find()
-
-    item = {}
-    data = []
-    for todo in _todos:
-        item = {
-            'id': str(todo['_id']),
-            'todo': todo['todo']
-        }
-        data.append(item)
-
-    return jsonify(
-        status=True,
-        data=data
-    )
-
-
-@application.route('/todo', methods=['POST'])
-def createTodo():
-    data = request.get_json(force=True)
-    item = {
-        'todo': data['todo']
-    }
-    db.todo.insert_one(item)
-
-    return jsonify(
-        status=True,
-        message='To-do saved successfully!'
-    ), 201
